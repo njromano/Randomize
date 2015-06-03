4c313a13008067ae3132911091c9fd5f38d95e7e
Nicholas Romano <xbloodnthunder@gmail.com>
2015-06-02 22:27:34 -0400

    Added some Intent passing between Main and EditList. I'm learning and things!

--- app/src/main/java/com/example/nick/randomize/EditList.java
+++ app/src/main/java/com/example/nick/randomize/EditList.java
@@ -1,9 +1,17 @@
 package com.example.nick.randomize;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class EditList extends ActionBarActivity {
@@ -12,6 +20,35 @@ public class EditList extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_list);
+
+        // fetch intent from MainActivity
+        Intent intent = getIntent();
+        String chosenItem = intent.getStringExtra(MainActivity.EXTRA_CHOSEN);
+
+
+        // test array TODO load this from intent and storage
+        String[] editArray = new String[]{"edit0", "edit1", "edit2", "edit3", "edit4", "edit5", "edit6",
+                "edit7", "edit8", "edit9", "edit10", "edit11", "edit12"};
+
+        // set up list array
+        ListView listView = (ListView) findViewById(R.id.listeditview);
+        ArrayList<String> editList = new ArrayList<String>();
+        for (int i = 0; i < editArray.length; i++) {
+            editList.add(editArray[i] + " " + chosenItem);
+        }
+
+        // set up ArrayAdapter to capture the array
+        ArrayAdapter editAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, editList);
+        listView.setAdapter(editAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "You clicked item" + position, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/nick/randomize/MainActivity.java
+++ app/src/main/java/com/example/nick/randomize/MainActivity.java
@@ -5,14 +5,48 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_CHOSEN = "com.example.nick.Randomize.CHOSEN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // test array of list items TODO make this or ArrayList load from storage
+        String[] array = new String[]{"test0", "test1", "test2", "test3", "test4", "test5", "test6",
+                "test7", "test8", "test9", "test10", "test11", "test12"};
+
+        // set up ListView to display the list items, along with ArrayList
+        ListView listView = (ListView) findViewById(R.id.listview);
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+
+        // set up ArrayAdapter to capture the array
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), EditList.class);
+                String chosen = Integer.toString(position);
+                intent.putExtra(EXTRA_CHOSEN, chosen);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
