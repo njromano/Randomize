76fa6d5659abfe5c0d1b09403b038001d3be1c33
Nicholas Romano <xbloodnthunder@gmail.com>
2015-06-03 23:07:52 -0400

    some title editing functionality

--- app/src/main/java/com/example/nick/randomize/EditList.java
+++ app/src/main/java/com/example/nick/randomize/EditList.java
@@ -1,6 +1,9 @@
 package com.example.nick.randomize;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -28,7 +31,7 @@ public class EditList extends ActionBarActivity {
 
         // fetch intent from MainActivity
         Intent intent = getIntent();
-        chosenList = (List) intent.getParcelableExtra(MainActivity.EXTRA_CHOSEN);
+        chosenList = intent.getParcelableExtra(MainActivity.EXTRA_CHOSEN);
 
         // set activity title
         setTitle(chosenList.title);
@@ -76,6 +79,9 @@ public class EditList extends ActionBarActivity {
             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
             intent.putExtra(EXTRA_SAVED, chosenList);
             startActivity(intent);
+        } else if (id == R.id.action_edit_title) {
+            showEditTitleDialog();
+            editAdapter.notifyDataSetChanged();
         }
 
         return super.onOptionsItemSelected(item);
@@ -89,5 +95,31 @@ public class EditList extends ActionBarActivity {
         ListView listView = (ListView) findViewById(R.id.listeditview);
         editAdapter.notifyDataSetChanged();
         newItem.setText("");
+    }
+
+    public void showEditTitleDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        final EditText edittext = new EditText(this);
+        alert.setTitle("Enter new title");
+        alert.setMessage("");
+        edittext.setText(chosenList.title);
+        alert.setView(edittext);
+
+        alert.setPositiveButton("Save Title", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                chosenList.title = edittext.getText().toString();
+                setTitle(chosenList.title);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+        alert.show();
     }
 }
